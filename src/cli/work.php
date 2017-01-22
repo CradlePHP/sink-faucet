4b5ed3b2244b29b05fe586b22372e826dc29875a
Christian Blanquera <cblanquera@openovate.com>
2017-01-22 12:15:53 +0800

    work modes
    - moved logic to cradle-queue
    - added modes: fork, exec

--- src/cli/work.php
+++ src/cli/work.php
@@ -7,13 +7,6 @@
  * distributed with this package.
  */
 
-use Cradle\CommandLine\Index as CommandLine;
-use PhpAmqpLib\Exception\AMQPProtocolChannelException;
-
-if (!defined('WORKER_ID')) {
-    define('WORKER_ID', md5(uniqid()));
-}
-
 /**
  * CLI starts worker
  *
@@ -21,23 +14,6 @@ if (!defined('WORKER_ID')) {
  * @param Response $response
  */
 return function ($request, $response) {
-    static $channel = null;
-
-    //get the channel
-    if (is_null($channel)) {
-        //add a logger
-        if($request->hasStage('v')) {
-            $this->addLogger(function ($message) {
-                echo '[cradle] ' . $message . PHP_EOL;
-            });
-        }
-
-        $channel = $this
-            ->package('global')
-            ->service('rabbitmq-main')
-            ->channel();
-    }
-
     //get the queue name
     $name = 'queue';
     if($request->hasStage(0)) {
@@ -46,104 +22,29 @@ return function ($request, $response) {
         $name = $request->getStage('name');
     }
 
-    // notify its up
-    $this->log('Waiting for tasks.');
-
-    // define the job
-    $job = function ($message) use ($name, $request, $response) {
-        // notify once a task is received
-        $this->log('A task is received.');
-
-        // get the data
-        $data = json_decode($message->body, true);
-
-        // extract the job to perform
-        if (!isset($data['__TASK__'])) {
-            // once an exception is encountered, notify that task is not done
-            $this->log('Task is not done.');
-
-            // set or flag that the task is not done and the worker is free
-            $message
-                ->delivery_info['channel']
-                ->basic_nack($message->delivery_info['delivery_tag']);
-
-            // set or flag that the task is not done and the worker is free and requeue task
-            //$message->delivery_info['channel']->basic_nack($message->delivery_info['delivery_tag'], false, true);
-        }
-
-        $task = $data['__TASK__'];
-        unset($data['__TASK__']);
-
-        try {
-            //start
-            $this->log($task . ' is running');
-
-            $command = sprintf(
-                'cd %s && bin/cradle %s%s --__worker_id=%s --__json=\'%s\'',
-                $request->getServer('PWD'),
-                $task,
-                $request->hasStage('v') ? ' -v': '',
-                WORKER_ID,
-                str_replace("'", "\\'", json_encode($data))
-            );
-
-            $this->log('Converting to the following command:');
-            $this->log($command);
-
-            system($command, $result);
-
-            if(!$result) {
-                $this->log('Task is not done.');
-                $this->log(json_encode($data));
-            } else {
-                $this->log($task . ' was performed');
-                $this->log(json_encode($data));
-
-                // once done, notify again, that it is done
-                $this->log('Task is done.');
-            }
-
-            // set or flag that the worker is free
-            $message
-                ->delivery_info['channel']
-                ->basic_ack($message->delivery_info['delivery_tag']);
-        } catch (Throwable $e) {
-            // once an exception is encountered, notify that task is not done
-            $this->log('Task is not done.');
-            $this->log($e->getMessage());
-
-            // set or flag that the task is not done and the worker is free
-            $message
-                ->delivery_info['channel']
-                ->basic_nack($message->delivery_info['delivery_tag']);
-        }
-    };
-
-    // worker consuming tasks from queue
-    $channel->basic_qos(null, 1, null);
-
-    // now we need to catch the channel exception
-    // when task does not exists in our queue
-    try {
-        // comsume messages on queue
-        $channel->basic_consume(
-            $name,
-            '',
-            false,
-            false,
-            false,
-            false,
-            $job->bindTo($this)
-        );
-    } catch (AMQPProtocolChannelException $e) {
-        // notify that task does not exists
-        $this->log('Task does not exists, creating task. Please re-run the worker.');
-
-        // create the init queue
-        $this->package('global')->queue('init');
+    $verbose = false;
+    if($request->hasStage('v') || $request->hasStage('verbose')) {
+        $verbose = true;
+        cradle()->addLogger(function($message) {
+            echo $message . PHP_EOL;
+        });
     }
 
-    while (count($channel->callbacks)) {
-        $channel->wait();
+    $mode = 'work';
+    if($request->hasStage('m')) {
+        $mode = $request->getStage('m');
+    } else if($request->hasStage('mode')) {
+        $mode = $request->getStage('mode');
     }
+
+    switch($mode) {
+        case 'fork':
+            $mode = 'workFork';
+            break;
+        case 'exec':
+            $mode = 'workExec';
+            break;
+    }
+
+    cradle('global')->$mode($name, '[cradle]', $verbose);
 };
