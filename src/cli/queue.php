97b00799770d5f43ab16f5c013475851250713b2
Christian Blanquera <cblanquera@openovate.com>
2017-01-22 21:10:49 +0800

    updated queue cli

--- src/cli/queue.php
+++ src/cli/queue.php
@@ -8,6 +8,7 @@
  */
 
 use Cradle\CommandLine\Index as CommandLine;
+use Cradle\Framework\Queue\Service\RabbitMQService;
 
 /**
  * CLI queue - bin/cradle faucet queue auth-verify auth_slug=<email>
@@ -23,21 +24,47 @@ return function ($request, $response) {
         CommandLine::error('Not enough arguments. Usage: bin/cradle faucet queue event data');
     }
 
+    $resource = cradle('global')->service('rabbitmq-main');
+
+    if(!$resource) {
+        CommandLine::error('Unable to queue, check config/services.php for correct connection information.');
+    }
+
     $event = array_shift($data);
 
-    $priority = 'low';
+    $priority = 0;
     if (isset($data['priority'])) {
         $priority = $data['priority'];
         unset($data['priority']);
     }
 
-    $delay = false;
+    $delay = 0;
     if (isset($data['delay'])) {
         $delay = $data['delay'];
         unset($data['delay']);
     }
 
-    if (!cradle('global')->queue($event, $data, $priority, $delay)) {
-        CommandLine::error('Unable to queue, check config/services.php for correct connection information.');
+    $retry = 0;
+    if (isset($data['retry'])) {
+        $delay = $data['retry'];
+        unset($data['retry']);
     }
+
+    $resource = cradle('global')->service('rabbitmq-main');
+    $settings = cradle('global')->config('settings');
+
+    $queue = 'queue';
+    if(isset($settings['queue'])) {
+        $queue = $settings['queue'];
+    }
+
+    (new RabbitMQService($resource))
+        ->setQueue($queue)
+        ->setData($data)
+        ->setDelay($delay)
+        ->setPriority($priority)
+        ->setRetry($retry)
+        ->send($event);
+
+    CommandLine::info('Queued: `' . $event . '` into `' . $queue . '`');
 };
